07c76cbafb481bc6ff8da847e12a738c80699895
Npi2015 <cuco20011@gmail.com>
2020-05-22 12:57:43 +0100

    Basic bot

--- __main__.py
+++ __main__.py
@@ -1,4 +1,19 @@
-import logging
+import discord
+
+token = "NzEyOTk3ODYyMjEyMTA4MzI2.XsZtgw.DlSDW7PoVNYPceFv3i4akmvS0XI"
 
 
-logging.log(logging.INFO, "Main run.")
+class Client(discord.Client):
+    async def on_ready(self):
+        print("Ready.")
+
+    async def on_message(self, message):
+        print(message)
+
+        if not message.author == self.user:
+            await message.channel.send("Hi")
+
+        print("Message sent")
+
+
+Client().run(token)
